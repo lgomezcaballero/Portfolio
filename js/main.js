26a7d511f3e9e1334b8e0c714056b630a77a8dbb
Leandro Gómez <94870514+lgomezcaballero@users.noreply.github.com>
2022-05-21 00:53:33 -0300

    Primera Subida

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const nav = document.querySelector('.nav');
+
+    window.addEventListener('scroll', function(){
+        nav.classList.toggle('active', window.scrollY >0)
+    })
+
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class = "lenguajes">C/C++</i>',
+        '<i class = "lenguajes">HTML y CSS</i>',
+        '<i class = "lenguajes">C# en .NET Framework</i>',
+        '<i class = "lenguajes">SQL SERVER</i>'
+    ],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+    typeSpeed: 110, // Velocidad en mlisegundos para poner una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    backSpeed: 55, // Velocidad en milisegundos para borrrar una letra,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    shuffle: false, // Alterar el orden en el que escribe las palabras.
+    backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+    loop: true, // Repetir el array de strings
+    loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+    showCursor: false, // Mostrar cursor palpitanto
+    //cursorChar: '|', // Caracter para el cursor
+    contentType: 'html', // 'html' o 'null' para texto sin formato
+});
